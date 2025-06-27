5c60a6ea7c0a371cf70ded75f27b073425550e0c
steven <21029341+StevenUS@users.noreply.github.com>
2025-06-26 19:52:18 -0500

    Remove dist directory from version control

--- dist/plugins/user.plugin.js
+++
@@ -1,7 +0,0 @@
-import fp from 'fastify-plugin';
-import { UserService } from '../services/user.service.js';
-const userServicePlugin = async (fastify) => {
-    fastify.decorate('userService', new UserService(fastify.db));
-};
-export default fp(userServicePlugin);
-//# sourceMappingURL=user.plugin.js.map
